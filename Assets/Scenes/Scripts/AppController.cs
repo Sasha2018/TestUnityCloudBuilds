8e71aebe44fc0b9e3b3208d58ec0a80f42a4dcdb
Sasha2018 <o.halinskyi@globalgames.net>
2018-05-01 10:35:04 +0300

    base commit

---
+++ Assets/Scenes/Scripts/AppController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum AppType
+{
+    Client,
+    Server
+}
+
+public class AppController : MonoBehaviour
+{
+    [SerializeField]
+    private Text _txtAppType;
+    [SerializeField]
+    private AppType _appType;
+
+    private void Update()
+    {
+        _txtAppType.text = _appType.ToString();   
+    }
+}
