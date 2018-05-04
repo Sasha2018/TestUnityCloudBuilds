1b409dd5a3972e0a3d2489962a14c625230c7b2b
Sasha_Halinskyi <soyellowbear@gmail.com>
2018-05-04 11:47:11 +0300

    Move to 2018 version Add difen symbols

--- Assets/Scenes/Scripts/AppController.cs
+++ Assets/Scenes/Scripts/AppController.cs
@@ -14,6 +14,15 @@ public class AppController : MonoBehaviour
     [SerializeField]
     private AppType _appType;
 
+    private void Start()
+    {
+#if CLIENT_BUILD
+        _appType = AppType.Client;
+#elif SERVER_BUILD
+        _appType = AppType.Server;
+#endif
+    }
+
     private void Update()
     {
         _txtAppType.text = _appType.ToString();   
